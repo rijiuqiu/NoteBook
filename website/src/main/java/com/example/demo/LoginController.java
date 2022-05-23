cc6d3b2c58c96a9923bfe80515980cb733a48126
rijiuqiu <104184129+rijiuqiu@users.noreply.github.com>
2022-05-23 17:58:51 +0900

    5.23update
    后端的跳转功能实现

---
+++ website/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class LoginController {
+	
+	
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public ModelAndView login(@RequestParam("username") String username,@RequestParam("password") String password,
+			ModelAndView mv) {
+	
+	
+		
+		
+		mv.addObject("username", username);
+		if(username.equals("admin") && password.equals("123456")) {
+			mv.setViewName("homepage");
+			
+		} else {
+			mv.setViewName("fail");
+		}
+		return mv;
+	}
+	
+	
+}
