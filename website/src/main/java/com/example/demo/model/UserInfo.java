36175b1d3d3be2f207ad4068679745ee80c0d696
rijiuqiu <104184129+rijiuqiu@users.noreply.github.com>
2022-05-24 17:59:00 +0900

    5.24update
    链接上数据库
    修改调整了编辑页面

--- website/src/main/java/com/example/demo/model/UserInfo.java
+++ website/src/main/java/com/example/demo/model/UserInfo.java
@@ -1,23 +1,23 @@
-package com.example.demo.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-
-
-import lombok.Data;
-
-@Data
-@Entity
-public class UserInfo {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	private String name;
-	
-	private String password;
-
-}
+//package com.example.demo.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//
+//
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//public class UserInfo {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Long id;
+//	
+//	private String name;
+//	
+//	private String password;
+//
+//}
--- website/src/main/java/com/example/demo/repository/UserInfoRepository.java
+++ website/src/main/java/com/example/demo/repository/UserInfoRepository.java
@@ -1,10 +1,10 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.UserInfo;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo,Long>{
-	
-
-}
+//package com.example.demo.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.example.demo.model.UserInfo;
+//
+//public interface UserInfoRepository extends JpaRepository<UserInfo,Long>{
+//	
+//
+//}
